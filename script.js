b575b15d1602cb5c463444b3a473f0c0f9154fcb
mohamed ibourki <mohamedibourkioffice@gmail.com>
2024-04-30 00:58:26 +0100

    update: front end

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function openNavMenu() {
-    const mobileListOfLinks = document.getElementById('mobileListOfLinks');
-    mobileListOfLinks.style.display = 'block';
-  }
-  
-  function closeNavMenu() {
-    const mobileListOfLinks = document.getElementById('mobileListOfLinks');
-    mobileListOfLinks.style.display = 'none';
-  }
+  const mobileListOfLinks = document.getElementById('mobileListOfLinks');
+  mobileListOfLinks.style.display = 'block';
+}
+
+function closeNavMenu() {
+  const mobileListOfLinks = document.getElementById('mobileListOfLinks');
+  mobileListOfLinks.style.display = 'none';
+}
+
+function filterClass(element) {
+  const allElements = document.querySelectorAll('.h3InsideClass');
+  allElements.forEach((el) => {
+    el.classList.remove('mainClass');
+  });
+  element.classList.add('mainClass');
+}
